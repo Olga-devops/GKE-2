c38205995ef0080901e3410251c64c77708a03e2
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-14 17:02:50 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("${var.google_credentials}")}"              #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
-  project     = "${var.google_project_id}"
-  region      = "${var.region}"
+  credentials = file("${var.google_credentials}")            #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
+  project     = var.google_project_id
+  region      = var.region
 }
